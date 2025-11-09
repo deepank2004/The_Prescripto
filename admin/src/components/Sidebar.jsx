93f18f6be4deef9636edc81f8b50595c47d9ec88
Deepank Sharma <gaurdeepank0@gmail.com>
2025-11-09 22:12:14 +0530

    Admin panel updated

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
                 <p>Add Doctor</p>
             </NavLink>
 
-            <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5F6FFF]' : ''}`} to={'/doctors-list'}>
+            <NavLink className={({isActive})=> `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-[#5F6FFF]' : ''}`} to={'/doctor-list'}>
                 <img src={assets.people_icon} alt="" />
                 <p>Doctor's List</p>
             </NavLink>

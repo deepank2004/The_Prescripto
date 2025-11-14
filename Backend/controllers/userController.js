866235a032419ab445ecead1ba5ce4319aa63877
Deepank Sharma <gaurdeepank0@gmail.com>
2025-11-14 22:14:19 +0530

    Created api to book appointment

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
 import { v2 as cloudinary } from 'cloudinary';
+import doctorModel from '../models/doctorModel.js';
 
 // ===============================
 // 🔹 REGISTER USER
@@ -136,3 +137,57 @@ export const updateProfile = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+//API to book appointment
+export const bookAppointment = async (req, res) => {
+
+  try {
+
+    const {userId, docId, slotDate, slotTime} = req.body;
+    const docData = await doctorModel.findById(docId).select('-password')
+
+    if(!docData.available){
+      return res.json({success:false , message:"Doctor not available"})
+    }
+
+    let slots_booked = docData.slots_booked
+
+    //checking for slot availability
+    if(slots_booked[slotDate]){
+      if(slots_booked[slotDate].includes(slotTime)){
+        return res.json({success:false , message:"Slot not available"})
+      } else {
+        slots_booked[slotDate].push(slotTime)
+      }
+    } else {
+      slots_booked[slotDate] = []
+      slots_booked[slotDate].push(slotTime)
+    }
+
+    const userData = await userModel.findById(userId).select('-password')
+    delete docData.slots_booked
+
+    const appointmentData = {
+      userId,
+      docId,
+      userData,
+      docData,
+      amount : docData.fees,
+      slotTime,
+      slotDate,
+      date: Date.now()
+    }
+
+    const newAppointment = new appointmentModel(appointmentData)
+    await newAppointment.save()
+
+    //save new slots data in docData
+    await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+    res.json({success:true , message: "Appointment booked successfully"})
+    
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+
+}
